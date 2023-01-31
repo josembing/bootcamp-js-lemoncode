9b30dc95773eff61685e96efc9ca2b7769b85a23
Jose <josemanuelamodelosrios@hotmail.com>
2023-01-31 18:32:10 +0100

    entrega modulo 1

---
+++ modulo-1/js/demo.js
@@ -0,0 +1,13 @@
+document.getElementById("name").value = "Bruce";
+document.getElementById("lastName").value = "Wayne";
+document.getElementById("city").value = "Gotham City";
+document.getElementById("address").value = "Batcueva / Mansion Wayne";
+
+document.getElementById("avatar").src = "C:\\Users\\Jose Manuel Amo\\Desktop\\playground 02\\img\\Que-Batman-eres-Portada.png";
+
+function showinputsvalues () {
+    console.log(document.getElementById('name').value + ' ' + document.getElementById('lastName').value + ', ' + document.getElementById('city').value)
+    
+
+    
+}
